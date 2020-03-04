43a227e3137c1701dce0356fc4bc59fccd213d0d
arcsridhar <61038056+arcsridhar@users.noreply.github.com>
2020-03-04 17:59:55 -0600

    Update adspost.php

--- adspost.php
+++ adspost.php
@@ -47,7 +47,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-2"><a class="navbar-brand" href="index.php">Bikroy.com</a></div>
-                <div class="col-md-3 pdt"><a href="allads.php"><button class="btn btn-info " type="submit">all ads</button></a> <button class="btn btn-info" type="submit">Bangla</button></div>
+                <div class="col-md-3 pdt"><a href="allads.php"><button class="btn btn-info " type="submit">All Advertisements</button></a> </div>
                 <div class="col-md-4 pdt pull-right text-right">
                  <?php if (isset($_SESSION['username'])) { ?>
 				<h5 class="navbar-text">Signed in as <?php echo $_SESSION['username']; ?></h5>
