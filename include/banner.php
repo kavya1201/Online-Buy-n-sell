2c421840531059cfa554de894e29d0236ef1bce8
kavya1201 <56418793+kavya1201@users.noreply.github.com>
2020-04-22 14:16:39 -0500

    Add files via upload

---
+++ include/banner.php
@@ -0,0 +1,5 @@
+    <section id="banner">
+        <div class="container banner">
+            <img class="img-responsive" src="images/1.png">
+        </div>
+    </section>
---
+++ include/header1.php
@@ -0,0 +1,13 @@
+<header id="header">
+       <div class="container">
+           <div class="row">
+               <div class="col-md-2"><a class="navbar-brand" href="index.php" style="color:white"><h5><b>OFFERUP</b></h5></a></div>
+               <div class="col-md-3 pdt"><a href="catWiseItems.php?id=1"><button class="btn btn-info " type="submit">All Advertisements</button></a> </div>
+               <div class="col-md-7 pdt pull-right text-right">
+       <a  class="kamal" href="logout.php"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Log Out</a>
+               <a href="adspost.php"> <button class="btn btn-warning sadi" type="submit">Post Your Ads </button></a>
+              </div>
+
+         </div>
+       </div>
+   </header>
